caea1d8effc32cb3b86c22334bf88bff60773b7c
Rogério <ramorim83@gmail.com>
2023-08-16 10:21:49 -0300

    add operators

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,95 @@
+import java.util.Date;
+
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        // ---------------- TIPOS PRIMITIVOS ------------------
+        byte um = 123;
+        Short dois = 1234;
+        int tres = 12345;
+        long quatro = 123L;
+
+        float quinto = 123.45f;
+        double sexto = 123.45d;
+
+        char setimo = '\u004a'; // , 'j';
+        boolean oitavo = true;
+
+        double salarioMinimo = 1320;
+
+        Date dtaNasc = new Date(); // neste caso, e necessario usar o import da biblioteca Date (ex:import
+                                   // java.util.Date) no inicio do programa.
+
+        short numCurto = 1;
+        int numNormal = numCurto;
+        // neste caso ao tentar converter um int para short e necessario fazer o
+        // casting, colocando o tipo da variavel antes do conteudo int.
+        short numCurto2 = (short) numNormal;
+
+        // exemplo de variavel que podem ser alteradas no decorrer do programa.
+        int numero = 1;
+        numero = 2;
+
+        // exemplo de variavel que não pode sofrer modificacao, neste caso devemos usar
+        // palavra reservada 'final' e colocar a variavel em uppercase.
+
+        final int NUMERO99 = 100;
+        // NUMERO99 = 10; neste caso apresentara um erro na execucao
+
+        System.out.println(NUMERO99);
+
+        // ----------------------------------------------------------------------------------------
+
+        // -------------- CONDICIONAL ------------------------------
+        int hora = 19;
+
+        if (hora < 12) {
+            System.out.println(dois);
+        } else if (hora < 18) {
+            System.out.println(dois);
+        } else {
+            System.out.println(dois);
+        }
+
+        char simbolo = '#';
+
+        switch (simbolo) {
+            case '$':
+                System.out.println("Dola");
+                break;
+            case '#':
+                System.out.println("Cerquilha");
+                break;
+            case '@':
+                System.out.println("Arroba");
+                break;
+            default:
+                System.out.println("Simbolo nao encontrado");
+        }
+
+        // ------------- LOOPS ---------------
+        var i = 0;
+
+        // WHILE
+        while (i < 100) {
+            i++;
+            System.out.println("I:" + i);
+        }
+
+        // Do
+        do {
+            i++;
+            System.out.println("I:" + i);
+        } while (i < 100);
+
+        // FOR
+        for (int j = 0, o = 100; j < o; j += 2, o -= 3) {
+
+            System.out.println("O:" + o);
+            System.out.println("J:" + j);
+
+        }
+
+        System.out.println(dois);
     }
 }
---
+++ tipos-variaveis/src/operadores.java
@@ -0,0 +1,41 @@
+public class operadores {
+    public static void main(String[] args) {
+        // ------------- OPERADORES ------------------
+        // ATRIBUICAO =
+        double calc = 0;
+
+        // ADICAO +
+        double somar = 10.5 + 15.7;
+
+        // SUBTRACAO -
+        int subtrair = 113 - 25;
+
+        // MULTIPLICACAO *
+        int multiplicar = 20 * 7;
+
+        // DIVISAO /
+        int dividir = 15 / 3;
+
+        // RESTO DE DIVISAO %
+        int modulo = 18 % 3;
+
+        // REGRA DE PRECEDENCIA
+        double resultado = (10 * 7) + (20 / 4);
+
+        // Incremento ++
+        // Decremento --
+        // Logica ? :
+
+        // -------------- OPERADORES LOGICOS ---------------------
+        // MENOR QUE <
+        // MENOR OU IGUAL <=
+        // MAIOR QUE >
+        // MAIOR OU IGUAL >=
+        // IGUAL ==
+        // DIFERENTE !=
+
+        // && AND
+        // || OR
+        // ! NOT
+    }
+}
