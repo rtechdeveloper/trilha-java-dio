0917daa51d79895ab4c3d47664c619a0b80ec030
Rogério <ramorim83@gmail.com>
2023-08-17 09:31:39 -0300

    settings usuario

--- smart-tv/src/Usuario.java
+++ smart-tv/src/Usuario.java
@@ -4,37 +4,39 @@ public class Usuario {
         // estanciando a classe SmartTv principal
         SmartTv smartTv = new SmartTv();
 
-        System.out.println("Qual o canal atual: " + smartTv.canal);
-        smartTv.mudarCanal(13);
+        // consumindo os metodos da classe SmartTv
 
         System.out.println("A TV está ligada: " + smartTv.ligada);
-        System.out.println("Qual o novo canal: " + smartTv.canal);
-        System.out.println("Qual o volume atual: " + smartTv.volume);
 
-        // consumindo os metodos da classe SmartTv
         smartTv.ligar();
         System.out.println("A TV está ligada: " + smartTv.ligada);
 
-        smartTv.desligar();
-        System.out.println("A TV está ligada: " + smartTv.ligada);
+        System.out.println("Qual o canal atual: " + smartTv.canal);
+        
+        smartTv.mudarCanal(13);
+        System.out.println("Qual o novo canal: " + smartTv.canal);
 
+        System.out.println("Qual o volume atual: " + smartTv.volume);
         smartTv.aumentarVolume();
         smartTv.aumentarVolume();
         smartTv.aumentarVolume();
         System.out.println("Você aumentou o volume: " + smartTv.volume);
 
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        System.out.println("Você subiu o canal: " + smartTv.canal);
+        
+        smartTv.diminuirCanal();
+        smartTv.diminuirCanal();
+        smartTv.diminuirCanal();
+        System.out.println("Você desceu o canal: " + smartTv.canal);
+
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         System.out.println("Você diminuiu o volume: " + smartTv.volume);
-
-        smartTv.aumentarCanal();
-        smartTv.aumentarCanal();
-        System.out.println("Você aumentou o canal: " + smartTv.canal);
-
-        smartTv.diminuirCanal();
-        smartTv.diminuirCanal();
-        smartTv.diminuirCanal();
-        System.out.println("Você diminuiu o canal: " + smartTv.canal);
+        
+        smartTv.desligar();
+        System.out.println("A TV está ligada: " + smartTv.ligada);    
 
     }
 }
