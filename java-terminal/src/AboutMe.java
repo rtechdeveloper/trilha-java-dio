57666cbed8f33b80f4c7e7d83ebc455037cf296d
Rogério Amorim <rtechdev.pt@gmail.com>
2023-08-17 22:47:07 -0300

    update AboutMe

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,10 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
         
-        String nome         = args[0];
-        String sobrenome    = args[1];
-        int idade           = Integer.valueOf(args[2]);
-        double altura       = Double.valueOf(args[3]);
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu Nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu Sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua Idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua Altura");
+        double altura = scanner.nextDouble();
 
         System.out.println("Olá, meu chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos" );
